2c869f6fe3a43e31b4fbb5dd1b49667afe2fdb6c
Abhishek Yelisetti <abhishekyelisetti56050@gmail.com>
2020-11-25 20:10:10 +0530

    Data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dish-detail/dish-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit , Input } from '@angular/core';
+import {Dish} from '../shared/dish';
+@Component({
+  selector: 'app-dish-detail',
+  templateUrl: './dish-detail.component.html',
+  styleUrls: ['./dish-detail.component.scss']
+})
+export class DishDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input()
+  dish : Dish;
+
+
+}
