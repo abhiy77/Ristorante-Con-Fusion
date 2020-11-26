c5156f1539ecb4b692a04df83c6cc5e170fe54c7
Abhishek Yelisetti <abhishekyelisetti56050@gmail.com>
2020-11-26 07:18:57 +0530

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,16 @@ import {DishService} from './services/dish.service';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
