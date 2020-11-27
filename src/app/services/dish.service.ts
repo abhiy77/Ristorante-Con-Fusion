fe30be05d4481dd90068576c4b7107657bada2fb
Abhishek Yelisetti <abhishekyelisetti56050@gmail.com>
2020-11-27 12:32:12 +0530

    Observables and rxjs Part - 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -50,4 +50,8 @@ export class DishService {
     //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
   }
 
+  getDishIds() : Observable<string[] | any>{
+      return of(DISHES.map(dish => dish.id));
+  }
+
 }
